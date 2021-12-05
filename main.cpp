51aa15538720695e7fc449722ca56ffe37201fdc
Michael Evans <Michael.evans1775@outlook.com>
2021-12-05 16:26:02 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+#include "userProfile.h"
+
+int main()
+{
+
+    float protein;
+    float fat; 
+    float carb;
+
+    userProfile user;
+
+    cout << "Welcome! Please login or create an account to continue" << endl;
+    cout << "1: login"
+         << "2: sign up" << endl;
+    int login;
+    cin >> login;
+
+    if (login == 1)
+    {
+        user.exsists();
+    }
+    else if (login == 2)
+    {
+        user.saveUser();
+    }
+
+    else
+    {
+        cout << "INVALID INPUT" << endl;
+    }
+
+    if (user.exsists() == true)
+    {
+        //cout << "welcome to the worlds most ok nutrient calculator" << endl;
+        cout << "how would you like to continue?" << endl;
+
+        cout << "1: add food" << endl;
+        cout << "2: remove food" << endl;
+        cout << "3: change username" << endl;
+        cout << "4: display current macros" << endl;
+        cout << "5: Exit" << endl;
+        int condition;
+        cin >> condition;
+
+        switch (condition)
+        {
+        case 1:
+            user.addMeal();
+            break;
+
+        case 2:
+            // todo remove food
+            break;
+
+        case 3:
+            break;
+        // todo change UN
+        case 4:
+            user.printUser(protein, fat, carb);
+            break;
+
+        case 5:
+            return 0;
+            break;
+        }
+    }
+}
---
+++ userProfile.h
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+class userProfile
+{
+private:
+public:
+    userProfile();
+    ~userProfile();
+    void printUser(float protien, float carb, float fat);
+    void saveUser();
+    void changeName();
+    void removeMeal();
+    void addMeal();
+    bool exsists();
+
+    string name;
+    int age;
+    int height;
+    int weight;
+    string sex;
+
+    float proteinGoal;
+    float carbGoal;
+    float fatGoal;
+    float calorieGoal;
+
+    string foodName;
+    float protein;
+    float fat;
+    float carb;
+   
+};
+
+void userProfile::saveUser()
+{
+    cout << setw(80) << setfill('-') << "" << setfill(' ') << endl;
+    cout << endl
+         << setw(25) << ""
+         << "Hello, tell us about yourself" << endl
+         << endl;
+    cout << "Please enter your first name: ";
+    cin >> name;
+    cout << "Please enter your weight: ";
+    cin >> weight;
+    cout << "Please enter your height: ";
+    cin >> height;
+    cout << "Please enter your age: ";
+    cin >> age;
+    cout << "Please enter your sex: ";
+    cin >> sex;
+    cout << "\n";
+
+    cout << setw(80) << setfill('-') << "" << setfill(' ') << endl;
+    cout << endl
+         << setw(25) << ""
+         << "Lets get to know your goals!" << endl
+         << endl;
+    cout << "What is your protien goal: ";
+    cin >> proteinGoal;
+    cout << "What is your carb goal: ";
+    cin >> carbGoal;
+    cout << "What is your fat goal: ";
+    cin >> fatGoal;
+    cout << "\n";
+
+    calorieGoal = (proteinGoal * 9) + (carbGoal * 4) + (fatGoal * 4);
+
+    cout << "your calculated calorie goal based on this info is: " << calorieGoal << endl;
+
+    ofstream write;
+
+    write.open("user.txt", ios::app);
+
+    if (!write.is_open())
+    {
+        cout << "cannot open file" << endl;
+    }
+
+    if (write.is_open())
+    {
+        write << name << " " << weight << " " << height << " " << age << " " << sex << " " << proteinGoal << " " << carbGoal << " " << fatGoal << " " << calorieGoal << endl;
+    }
+
+    write.close();
+}
+bool userProfile::exsists()
+{
+
+    ifstream inFile;
+
+    inFile.open("user.txt");
+
+    if (!inFile)
+    {
+        cout << "Cannot open input file." << endl;
+    }
+    string userName;
+
+    cout << "What is the name that is attached to the account?" << endl;
+    cin >> userName;
+
+    int curLine;
+    string line;
+    string temp;
+
+    while (getline(inFile, line))
+    {
+
+        if (line.find(userName, 0) != string::npos)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+void userProfile::addMeal()
+{
+    
+
+    ifstream inFile;
+
+    vector<string> foodLog;
+
+    inFile.open("food.txt");
+
+    if (!inFile)
+    {
+        cout << "Cannot open input file." << endl;
+        return;
+    }
+    string search;
+
+    cout << "Which food would you like to search" << endl;
+    cin >> search;
+
+    int curLine;
+    string line;
+    string temp;
+    bool searchFinished;
+
+    while (getline(inFile, line))
+    {
+
+        if (line.find(search, 0) != string::npos)
+        {
+            cout << "found: " << search << " Macronutrients " << line << endl;
+            // searchFinished = true;
+        }
+    }
+
+    while (getline(inFile, line))
+    {
+
+        inFile >> temp;
+        if (temp == search)
+        {
+            foodName = temp;
+            inFile >> protein;
+            inFile >> carb;
+            inFile >> fat;
+        }
+        // foodLog.push_back(foodName);
+        // foodLog.push_back(protein);
+        // foodLog.push_back(carb);
+        // foodLog.push_back(fat);
+    }
+
+    inFile.close();
+
+    cout << "If we are displaying multiple results please refine your search below" << endl;
+    cout << "Please press Y to refine search" << endl;
+    cout << "Please Press N to confirm selection" << endl;
+    string refine;
+    cin >> refine;
+
+    if (refine == "y" || refine == "Y")
+    {
+        ifstream inFile;
+        string search2;
+        inFile.open("food.txt");
+        cin >> search2;
+        while (getline(inFile, line))
+        {
+            inFile >> temp;
+            if (temp == search2)
+            {
+                foodName = temp;
+                inFile >> protein;
+                inFile >> carb;
+                inFile >> fat;
+            }
+            curLine++;
+            if (line.find(search2, 0) != string::npos)
+            {
+                cout << "found: " << search2 << " Macronutrients " << line << endl;
+            }
+        }
+        // foodLog.push_back(foodName);
+        // foodLog.push_back(protein);
+        // foodLog.push_back(carb);
+        // foodLog.push_back(fat);
+
+        inFile.close();
+    }
+
+    else if (refine == "n" || refine == "N")
+    {
+        cout << "Thanks for adding that food!" << endl;
+    }
+    else
+    {
+        cout << "incorrect input" << endl;
+    }
+
+    cout << "Would you like to add another food to this meal? " <<endl;
+    string additionalFood;
+    cin>>additionalFood;
+
+    if (additionalFood == "Y" || additionalFood == "y"){
+        addMeal();
+    }
+
+    else if( additionalFood == "N" || additionalFood == "N"){
+        cout << "thanks! your values are:" <<endl;
+
+        printUser(protein,fat,carb);
+    }
+}
+void userProfile::printUser(float protein,float fat,float carb)
+{
+    cout << "Macros: " << endl;
+    cout << "Caloric goal: " << calorieGoal << "Current Calories" << "todo" <<endl;
+    cout << " Protein goal: " << proteinGoal <<"Current Protein: " << protein << endl;
+    cout << "Carb goal: " << carbGoal << "Current carbs: " << carb << endl;
+    cout << "fat goal: " << fatGoal << "CUrrent fat: " << fat << endl;
+}
+userProfile::userProfile()
+{
+}
+
+userProfile::~userProfile()
+{
+}
